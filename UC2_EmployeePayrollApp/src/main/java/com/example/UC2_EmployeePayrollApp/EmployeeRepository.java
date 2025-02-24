6f3f228865069604b1b0fb35e3db575ea0ecc164
kalpesh doble <kalpeshdoble31@gmail.com>
2025-02-24 10:57:20 +0530

    [kalpesh]Add UC2_EmployeePayrollApp

--- UC2_EmployeePayrollApp/src/main/java/com/example/UC2_EmployeePayrollApp/EmployeeRepository.java
+++ UC2_EmployeePayrollApp/src/main/java/com/example/UC2_EmployeePayrollApp/EmployeeRepository.java
@@ -1,6 +1,4 @@
-package com.example.UC1_EmployeePayrollApp;
-
-
+package com.example.UC2_EmployeePayrollApp;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
